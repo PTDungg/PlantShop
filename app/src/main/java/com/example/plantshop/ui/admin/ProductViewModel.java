bca0437eceb3115c8dda2657389b1103afa2a258
PTDung <phamthedung0304@gmail.com>
2025-06-22 07:21:19 +0700

    feat(admin): add product

--- app/src/main/java/com/example/plantshop/ui/admin/ProductViewModel.java
+++ app/src/main/java/com/example/plantshop/ui/admin/ProductViewModel.java
@@ -53,6 +53,10 @@ public class ProductViewModel extends ViewModel {
             filteredProducts.setValue(filtered);
         }
     }
+    public void addProduct(Product product, Consumer<Boolean> callback) {
+        productRepository.addProduct(product, callback);
+        loadProducts();
+    }
     public void updateProduct(Product product, Consumer<Boolean> callback) {
         productRepository.updateProduct(product, callback);
         loadProducts();
