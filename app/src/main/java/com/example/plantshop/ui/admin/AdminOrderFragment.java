f3cd01032ee84e06a83e141aba87ed8812334d64
PTDung <phamthedung0304@gmail.com>
2025-06-22 07:25:53 +0700

    chore

--- app/src/main/java/com/example/plantshop/ui/admin/AdminOrderFragment.java
+++ app/src/main/java/com/example/plantshop/ui/admin/AdminOrderFragment.java
@@ -61,6 +61,6 @@ public class AdminOrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_noti, container, false);
+        return inflater.inflate(R.layout.fragment_admin_order, container, false);
     }
 }
